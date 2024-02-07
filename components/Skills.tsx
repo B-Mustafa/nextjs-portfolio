c2147c7f171472b48d7a106adae5351a99fb84a5
Mustafa <mustafabhikhpurwala@gmail.com>
2024-02-07 21:17:59 +0530

    added icons in place of text in skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,10 +1,23 @@
 "use client";
 
 import React from "react";
-
+import HTMLicon from "@/public/assets/html.svg"
+import CSSicon from "@/public/assets/css.svg"
+import JSicon from "@/public/assets/javascript.svg"
+import TSicon from "@/public/assets/typescript.svg"
+import REACTJSicon from "@/public/assets/react.svg"
+import NEXTJSicon from "@/public/assets/nextjs.svg"
+import NODEJSicon from "@/public/assets/nodejs.svg"
+import GITicon from "@/public/assets/git.svg"
+import TAILWINDCSSicon from "@/public/assets/tailwind-css.svg"
+import MONGODBicon from "@/public/assets/mongodb.png"
+import FIREBASEicon from "@/public/assets/firebase.svg"
+import EXPRESSJSicon from "@/public/assets/expressjs.svg"
+import PYTHONicon from "@/public/assets/python.svg"
 
 
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 const fadeInAnimationVariants = {
   initial: {
@@ -23,19 +36,19 @@ const fadeInAnimationVariants = {
 export default function Skills() {
 
     const SkillsData = [
-        "HTML",
-        "CSS",
-        "JavaScript",
-        "TypeScript",
-        "React",
-        "Next.js",
-        "Node.js",
-        "Git",
-        "Tailwind",
-        "MongoDB",
-        "Express",
-        "Python",
-        "Framer Motion",
+        {icon: HTMLicon},
+        {icon: CSSicon},
+        {icon: JSicon},
+        {icon: TSicon},
+        {icon: REACTJSicon},
+        {icon: NEXTJSicon},
+        {icon: NODEJSicon},
+        {icon: GITicon},
+        {icon: TAILWINDCSSicon},
+        {icon: PYTHONicon},
+        {icon: MONGODBicon},
+        {icon: FIREBASEicon},
+        {icon: EXPRESSJSicon},
     ]
 
 
@@ -43,7 +56,7 @@ export default function Skills() {
     <section
       id="skills"
       
-      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40 items-center "
     >
       <h2 className="text-3xl font-medium capitalize mb-8 text-left text-blue-700">My skills</h2>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
@@ -59,7 +72,7 @@ export default function Skills() {
             }}
             custom={index}
           >
-            {skill}
+            <Image src={skill.icon} alt="tech-icon" />
           </motion.li>
         ))}
       </ul>
